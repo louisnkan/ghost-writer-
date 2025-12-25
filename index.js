f6c0d4c5a40b686bad800f9f864581e0475bc57c
LOUIS NKAN ENOBONG <98494528+louisnkan@users.noreply.github.com>
2025-12-25 19:00:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const cors = require('cors');
+const cors = require('cors'); //
 const axios = require('axios');
 const app = express();
 
-app.use(cors());
+app.use(cors()); //
 app.use(express.json());
 
 const SECRET_KEY = "AIzaSyCeh62ba_6SuUoFDbdVp4eIzBAfDtWYvjM";
